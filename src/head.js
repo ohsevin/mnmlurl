8bcdce51ceb8154513aee07cdc372242550dd9a4
Cookie Monster <31463232+GhostRideTheGit@users.noreply.github.com>
2022-12-28 11:12:59 -0800

    Update head.js

--- src/head.js
+++ src/head.js
@@ -1,4 +1,4 @@
-const endpoint = 'https://www.jsonstore.io/6b6e3cd07f3aaecd8df25ab95875eb2462f00a7614f7205c2d5dc5229896ea98'
+const endpoint = 'https://www.jsonstore.io/00a4c82b645287a055779525d4fb2b7beb569111947fcc728d9148d625b8537a'
 const ebox = document.getElementById('erbox')
 const fetchJSON = (a) => {
 	const f = new XMLHttpRequest()
